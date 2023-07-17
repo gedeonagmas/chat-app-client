9fc0cb81ff8b8fe5300140648abff3133a2c7bbc
gedeonagmas <gedeonagmas2580@gmail.com>
2023-07-17 16:34:51 +0300

    first commit

---
+++ src/components/landing/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="fixed flex items-center justify-between px-4 z-30 font-bold top-0 left-0 h-14 w-[100%] bg-white">
+      <p className="text-lg font-bold text-black">gedion.dev</p>
+    </div>
+  );
+};
+
+export default Footer;
